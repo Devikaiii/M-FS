94706836265806098aea35e794a50262c2d7489b
Devikaiii <89661349+Devikaiii@users.noreply.github.com>
2022-07-07 18:54:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,2 +1,14 @@
-
-Mmmm
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                ],
+                [
+                    InlineKeyboardButton("BACK", callback_data="help_data"),
+                    InlineKeyboardButton("CLOSE", callback_data="close_data"),
+                ]                
+            ]
+        ),
+        reply_to_message_id=message.message_id
+    )
